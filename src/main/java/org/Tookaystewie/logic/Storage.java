0c51bec5559d9a75b8c2bbff14cf90662a0588a4
Toostew <tookaystewie@gmail.com>
2025-11-27 13:37:23 +0800

    added writeLine and listTasks to storage.java

--- src/main/java/org/Tookaystewie/logic/Storage.java
+++ src/main/java/org/Tookaystewie/logic/Storage.java
@@ -4,12 +4,18 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Storage {
+    //should probably change to private
     public HashMap<Integer, Tasks> taskMap = new HashMap<Integer, Tasks>();
 
-
+    public static void main(String[] args) {
+        Storage storage = new Storage();
+        storage.readFile();
+        storage.listTasks();
+    }
 
     public void taskFileExists() {
         try{
@@ -25,7 +31,7 @@ public class Storage {
         }
     }
 
-    //reads task storage file
+    //reads task.txt storage file
     public void readFile(){
         taskFileExists();
         File file = new File("data/tasks.txt");
@@ -50,13 +56,29 @@ public class Storage {
         //ID|NAME|DESC|COMPLETED
         //tasks(name, desc completed, id)
         String[] parts = line.split("\\|");
-        if(parts.length != 4){System.out.println("ERROR: line does NOT follow convention!");}
+
+        if(parts.length != 4){
+            System.out.println("ERROR: line does NOT follow convention!");
+            return null;
+        }
+
         return new Tasks(parts[1],parts[2],parts[3].equalsIgnoreCase("true"),Integer.valueOf(parts[0]));
     }
 
-    //lists every single task, seperated by comma
-    public void listTasks(){
+    public void writeLine(String name, String description, boolean completed, int id){
+        try(FileWriter fileWriter = new FileWriter("data/tasks.txt", true)){
+            fileWriter.write(id+"|"+name+"|"+description+"|"+completed + System.lineSeparator());
+        } catch (IOException e){
+            System.out.println("Error writing data to tasks.txt");
+        }
+    }
 
+    //lists every single task, seperated by comma, and list if completed
+    public void listTasks(){
+        System.out.println("Task List:");
+        for(Tasks task : taskMap.values()){
+            System.out.println(task.getName() + (task.getCompleted() == true ? "(COMPLETED)" : ""));
+        }
     }
 
 }
